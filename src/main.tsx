0a487e2ec117f2b8838c9b54f3c17b13a9f4ef35
kiiiyo <kiiiyo.jp@gmail.com>
2022-07-13 20:59:57 +0900

    wip: modify base app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { createApp } from 'solid-utils'
 import { MetaProvider } from 'solid-meta'
-//
 import { App } from './app'
-//
 import 'tailwindcss/tailwind.css'
-import './index.css'
+import './styles/index.css'
 
 createApp(App).use(MetaProvider).mount('#root')
