4f8f6d18bbc80367e357ffd454fc8c5967c8a462
kiiiyo <kiiiyo.jp@gmail.com>
2022-07-20 23:03:17 +0900

    ui: add About Section Comment

---
+++ src/components/organisms/about-section/about-section.component.tsx
@@ -0,0 +1,9 @@
+export const AboutSection = () => {
+  return (
+    <section>
+      <div class="w-ful">
+        <div class="container mx-auto text-white">About</div>
+      </div>
+    </section>
+  )
+}
--- src/components/pages/home-page/home-page.component.tsx
+++ src/components/pages/home-page/home-page.component.tsx
@@ -6,7 +6,11 @@ export const HomePage = ({}: Props) => {
   return (
     <Templates.GenericTemplate
       header={<Organisms.Header />}
-      content={<div>content</div>}
+      content={
+        <>
+          <Organisms.AboutSection />
+        </>
+      }
       footer={<Organisms.Footer />}
     />
   )
