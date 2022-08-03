207c47b4934e17ec3e12b8970fed1039cda08081
kiiiyo <kiiiyo.jp@gmail.com>
2022-08-03 20:40:36 +0900

    config: modify import lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,15 +10,37 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier'
   ],
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'import'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json']
   },
-  rules: {
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off'
-  }
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true
+      }
+    }
+  },
+  rules: {},
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {}
+    },
+    {
+      files: ['src/**/*.{js,jsx,ts,tsx}'],
+      rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error'
+      }
+    }
+  ]
 }
